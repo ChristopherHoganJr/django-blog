0c8cff47a9b31c156a0a7927e2df6355ef79084e
Chris <55262451+ChristopherHoganJr@users.noreply.github.com>
2023-10-20 13:37:27 -0700

    Blog Model

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Blog
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
 
 # Register your models here.
 admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
